522d54fb96375b6cd73e5bf6fdcf16ab1b0a210a
BearSwan <lebedeeev@gmail.com>
2018-02-25 02:08:41 +0200

    Bino-6: feature: history, services block

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
-		$('.js-hero').slick({
-			nextArrow: '.b-hero-slider_next',
-			prevArrow: '.b-hero-slider_prev'
-		})
+	$('.js-hero_slider').slick({
+		nextArrow: '.b-hero-slider_next',
+		prevArrow: '.b-hero-slider_prev'
+	})
+
+	$('.js-services_slider').slick({
+		dots: true,
+		arrows: false,
+		vertical: true,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		verticalSwiping: true
+	})
 });
